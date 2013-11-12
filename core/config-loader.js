0577264df0b9499db68afbb77ff0d68e0a19cf25
rektide <rektide@voodoowarez.com>
2013-11-12 00:00:42 -0500

    Prefer "configFile" and "config" variable names in config-loader. Towards #1110.

--- core/config-loader.js
+++ core/config-loader.js
@@ -6,7 +6,7 @@ var fs      = require('fs'),
 
     appRoot = path.resolve(__dirname, '../'),
     configexample = path.join(appRoot, 'config.example.js'),
-    config = process.argv[2] || path.join(appRoot, 'config.js');
+    configFile = process.argv[2] || path.join(appRoot, 'config.js');
 
 function writeConfigFile() {
     var written = when.defer();
@@ -28,7 +28,7 @@ function writeConfigFile() {
         });
         read.on('end', written.resolve);
 
-        write = fs.createWriteStream(config);
+        write = fs.createWriteStream(configFile);
         write.on('error', function (err) {
             return errors.logError(new Error('Could not open config.js for write.'), appRoot, 'Please check your deployment for config.js or config.example.js.');
         });
@@ -44,42 +44,42 @@ function validateConfigEnvironment() {
         hasHostAndPort,
         hasSocket,
         globalConf,
-        conf,
+        config,
         parsedUrl;
 
     try {
-        globalConf = require(config);
-        conf = globalConf[envVal];
+        globalConf = require(configFile);
+        config = globalConf[envVal];
     } catch (ignore) {
 
     }
 
 
     // Check if we don't even have a config
-    if (!conf) {
+    if (!config) {
         errors.logError(new Error('Cannot find the configuration for the current NODE_ENV'), "NODE_ENV=" + envVal, 'Ensure your config.js has a section for the current NODE_ENV value');
         return when.reject();
     }
 
     // Check that our url is valid
-    parsedUrl = url.parse(conf.url || 'invalid', false, true);
+    parsedUrl = url.parse(config.url || 'invalid', false, true);
     if (!parsedUrl.host) {
-        errors.logError(new Error('Your site url in config.js is invalid.'), conf.url, 'Please make sure this is a valid url before restarting');
+        errors.logError(new Error('Your site url in config.js is invalid.'), config.url, 'Please make sure this is a valid url before restarting');
         return when.reject();
     }
 
     // Check that we have database values
-    if (!conf.database) {
-        errors.logError(new Error('Your database configuration in config.js is invalid.'), JSON.stringify(conf.database), 'Please make sure this is a valid Bookshelf database configuration');
+    if (!config.database) {
+        errors.logError(new Error('Your database configuration in config.js is invalid.'), JSON.stringify(config.database), 'Please make sure this is a valid Bookshelf database configuration');
         return when.reject();
     }
 
-    hasHostAndPort = conf.server && !!conf.server.host && !!conf.server.port;
-    hasSocket = conf.server && !!conf.server.socket;
+    hasHostAndPort = config.server && !!config.server.host && !!config.server.port;
+    hasSocket = config.server && !!config.server.socket;
 
     // Check for valid server host and port values
-    if (!conf.server || !(hasHostAndPort || hasSocket)) {
-        errors.logError(new Error('Your server values (socket, or host and port) in config.js are invalid.'), JSON.stringify(conf.server), 'Please provide them before restarting.');
+    if (!config.server || !(hasHostAndPort || hasSocket)) {
+        errors.logError(new Error('Your server values (socket, or host and port) in config.js are invalid.'), JSON.stringify(config.server), 'Please provide them before restarting.');
         return when.reject();
     }
 
@@ -98,7 +98,7 @@ exports.loadConfig = function () {
         pendingConfig;
     /* Check for config file and copy from config.example.js
         if one doesn't exist. After that, start the server. */
-    fs.exists(config, function checkConfig(configExists) {
+    fs.exists(configFile, function checkConfig(configExists) {
         if (!configExists) {
             pendingConfig = writeConfigFile();
         }
